4fa3d9dd18b1e72279ed9f0d05b4a3816f64bb6e
okonomipizza <140386510+okonomipizza@users.noreply.github.com>
2025-01-31 22:22:58 +0900

    Add position() method to all Nodes

--- src/tree.rs
+++ src/tree.rs
@@ -11,6 +11,22 @@ pub enum Node {
     Italic(Italic),
     Bold(Bold),
     Whitespace(Whitespace),
+    Eol(Eol),
+}
+
+impl Node {
+    pub fn position(&self) -> &LineSpan {
+        match self {
+            Node::Header(header) => header.position(),
+            Node::Paragraph(paragraph) => paragraph.position(),
+            Node::UnorderedList(unordered_list) => unordered_list.position(),
+            Node::Text(text) => text.position(),
+            Node::Italic(italic) => italic.position(),
+            Node::Bold(bold) => bold.position(),
+            Node::Whitespace(whitespace) => whitespace.position(),
+            Node::Eol(eol) => eol.position(),
+        }
+    }
 }
 
 pub trait Positioned {
@@ -22,6 +38,24 @@ pub struct LineSpan {
     pub start: usize,
     pub end: usize,
 }
+
+macro_rules! impl_positioned {
+    ($struct_name:ident) => {
+        impl Positioned for $struct_name {
+            fn position(&self) -> &LineSpan {
+                &self.position
+            }
+        }
+    };
+}
+impl_positioned!(Header);
+impl_positioned!(Paragraph);
+impl_positioned!(UnorderedList);
+impl_positioned!(Text);
+impl_positioned!(Italic);
+impl_positioned!(Bold);
+impl_positioned!(Whitespace);
+impl_positioned!(Eol);
 
 #[derive(Debug, PartialEq, Eq, Serialize)]
 pub struct Header {
@@ -42,12 +76,6 @@ pub struct UnorderedList {
     pub nodes: Vec<Node>,
     pub children: Vec<Node>,
     pub position: LineSpan,
-}
-
-impl Positioned for UnorderedList {
-    fn position(&self) -> &LineSpan {
-        &self.position
-    }
 }
 
 #[derive(Debug, PartialEq, Eq, Serialize)]
